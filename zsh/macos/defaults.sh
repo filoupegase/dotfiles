d7fdd87bf09bc6198f337ce7e5cf40331bf1d3fb
filoupegase <loisoncorentin@yahoo.fr>
2023-05-16 12:35:08 +0200

    save

---
+++ zsh/macos/defaults.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+# modifie le temps de l'animation que prends les apps du doc a s'afficher + le reset
+#defaults write com.apple.dock autohide-delay -float 0
+#defaults write com.apple.dock autohide-time-modifier -float 0.3; killall Dock
