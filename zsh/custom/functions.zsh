e83fb3e7b78b60f7899c94223eeb6d95c7365a47
filoupegase <loisoncorentin@yahoo.fr>
2023-05-19 21:20:07 +0200

    rename functions file error name

---
+++ zsh/custom/functions.zsh
@@ -0,0 +1,7 @@
+#!/usr/bin/env zsh
+
+# Make a new directory and `cd` right into it (this seems like a no-brainer)
+mkcd() {
+  mkdir -p -- "$1" &&
+  cd -P -- "$1" || return
+}
--- zsh/custom/plugins.zsh
+++ zsh/custom/plugins.zsh
@@ -1,4 +1,28 @@
 #!/usr/bin/env zsh
 
 # check for zinit and clone if not installed
-#ZINIT_HOME="$HOME/.local/share"
+ZINIT_HOME="$HOME/.local/share/zinit/zinit.git"
+
+if [[ ! -d "$ZINIT_HOME" ]]; then
+  mkdir -p "$(dirname "$ZINIT_HOME")"
+  git clone https://github.com/zdharma-continuum/zinit.git "$ZINIT_HOME"
+fi
+
+# initialize zinit
+# shellcheck disable=SC1091
+source "$ZINIT_HOME/zinit.zsh"
+
+# ensure compinit recognizes zinit's changes
+autoload -Uz _zinit
+# shellcheck disable=SC2154
+# shellcheck disable=SC2034
+(( ${+_comps} )) && _comps[zinit]=_zinit
+
+# various zinit add-ons
+zinit light zdharma-continuum/zinit-annex-patch-dl
+
+# async support
+zinit ice wait lucid \
+  atload"async_init" \
+  src"async.zsh"
+zinit light mafredri/zsh-async
