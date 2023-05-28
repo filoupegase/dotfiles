b47d985b26bd66a773d8f8fb4711e74b5597c02f
filoupegase <loisoncorentin@yahoo.fr>
2023-05-29 00:50:57 +0200

    set linux file

---
+++ linux/linux.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+# set machine's timezone to local time
+sudo timedatectl set-timezone Europe/Paris
+
+# install linuxbrew:
+# https://docs.brew.sh/Homebrew-on-Linux#install
+if ! command -v brew &>/dev/null; then
+  # TODO: hack to permit installing as root, find a better way
+  # https://github.com/Homebrew/install/blob/master/install.sh#L306
+  sudo touch /.dockerenv
+  curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh | bash
+  sudo rm /.dockerenv
+fi
