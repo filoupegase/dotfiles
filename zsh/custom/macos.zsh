040a830f13532bb9ba63702e2a8feb362ded27a3
filoupegase <loisoncorentin@yahoo.fr>
2023-05-30 17:14:06 +0200

    func systme update

--- zsh/custom/macos.zsh
+++ zsh/custom/macos.zsh
@@ -8,11 +8,16 @@ system_update() {
   echo -e "${YELLOW}Updating Homebrew formulae and casks...${NC}"
   brew update
   brew upgrade
-
+  # avoid annoying `(latest) != latest` cask updates:
+  # shellcheck disable=SC2046
+  brew upgrade $(brew outdated --greedy --verbose | awk '$2 !~ /(latest)/ {print $1}')
   brew cleanup
-}
 
-# node, npm, yarn
+  # zinit & plugins
+  echo -e "${YELLOW}Updating zinit...${NC}"
+  zinit self-update
+  zinit update --all
+}
 
 # open current directory in Finder
 alias finder="open -a Finder ."
