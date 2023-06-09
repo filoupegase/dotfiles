5655398f619ca029de41316b461c35eead2ab228
filoupegase <loisoncorentin@yahoo.fr>
2023-06-09 21:36:46 +0200

    save

--- zsh/custom/common.zsh
+++ zsh/custom/common.zsh
@@ -11,13 +11,28 @@ select-word-style bash
 # enable bash-like 'help' command for zsh:
 # https://stackoverflow.com/questions/4405382/how-can-i-read-documentation-about-built-in-zsh-commands
 unalias run-help 2>/dev/null
-# Remove the default of run-help being aliased to man
 autoload run-help
-# Use zsh's run-help, which will display information for zsh builtins.
 alias help=run-help
 
 # ---
 
 setopt notify
+setopt complete_in_word
+setopt always_to_end
+setopt extended_history
+setopt hist_expire_dups_first
+# setopt hist_ignore_dups
+setopt hist_ignore_space
+setopt hist_verify
+setopt share_history
+setopt inc_append_history
+setopt rc_quotes
+setopt autocd
+setopt extended_glob
+setopt glob_dots
+unsetopt case_glob
+unsetopt beep  # kindly stfu
+unsetopt correct
+unsetopt correct_all
 
 # ---
