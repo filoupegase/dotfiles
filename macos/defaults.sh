16e7da79ff37c80544eda68fcfe687186b88d496
filoupegase <loisoncorentin@yahoo.fr>
2023-09-27 16:43:45 +0200

    save default macos opti

--- macos/defaults.sh
+++ macos/defaults.sh
@@ -65,6 +65,17 @@ defaults write com.apple.dock showLaunchpadGestureEnabled -int 0
 defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
 
 ###############################################################################
+# TextEdit                                                                    #
+###############################################################################
+
+# Use plain text mode for new TextEdit documents
+defaults write com.apple.TextEdit RichText -int 0
+
+# Open and save files as UTF-8 in TextEdit
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+###############################################################################
 # iTerm 2                                                                     #
 ###############################################################################
 
