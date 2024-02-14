52a9470cd6edff8befaab98467a73caf2c0b30b6
filoupegase <loisoncorentin@yahoo.fr>
2024-02-14 17:29:07 +0100

    fix rebasing

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -20,6 +20,21 @@ alias gc="git commit -m"  # + commit message
 alias gca="git add . && git commit -m"  # + commit message
 alias gs="git status -sb"
 alias gl="git log --pretty=short"
+alias gd="git diff"
+alias gds="git diff --staged"
+alias gpom="git push origin main"
+alias glom="git pull origin main"
+alias gpo="git push origin"  # + branch name
+alias glo="git pull origin"  # + branch name
+alias glfm="git fetch && git reset origin/main --hard"
+alias gb="git checkout"  # + existing branch name
+alias gbn="git checkout -b"  # + new branch name
+alias grm="git rebase -i origin/main"
+alias gsub="git submodule update --recursive --remote"
+alias gundo="git reset --soft HEAD~1"
+alias gres="git reset"
+alias github="gh repo view --web"
+alias gist="gh gist create --web"
 
 # Node/NPM/Yarn
 alias npr="npm run"
