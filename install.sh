fef77f3aa344d546b68e485047d8d9cde5b6a9a7
Corentin Loison <loisoncorentin@yahoo.fr>
2024-02-15 14:44:28 +0100

    forgot something

--- install.sh
+++ install.sh
@@ -16,6 +16,15 @@ if [[ "$CODESPACES" = "true" ]] && [[ -d /workspaces/.codespaces/.persistedshare
   ln -sf /workspaces/.codespaces/.persistedshare/dotfiles "$DOTFILES_PATH"
 fi
 
+# clone this repo if this script is all by itself and/or we're not in the expected location
+if [[ "$INSTALLER_PATH" != "$DOTFILES_PATH" ]] && [[ ! -d "$DOTFILES_PATH" ]]; then
+  git clone https://github.com/filoupegase/dotfiles.git "$DOTFILES_PATH"
+
+  echo "Successfully cloned the full repo to '$DOTFILES_PATH'"
+  echo "Run install.sh from that directory to continue. Exiting now..."
+  exit 0
+fi
+
 # set up symlinks from various default paths to files in this repo
 if [[ ! -d ~/.config ]]; then
   mkdir -p ~/.config
